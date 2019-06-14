a16fe03fc194486c5cccd19caf842afb58a6bd92
Ryan Crawford <RyanCCrawford@live.com>
2019-06-14 14:49:13 -0400

    Added .env support

---
+++ config/config.js
@@ -0,0 +1,7 @@
+exports.mysqlPrivate = {
+    usernameRemote: process.env.MYSQL_USER_REMOTE,
+    passwordRemote: process.env.MYSQL_PASSWORD_REMOTE,
+    usernameLocal: process.env.MYSQL_USER_LOCAL,
+    passwordLocal: process.env.MYSQL_PASSWORD_LOCAL,   
+  };
+  
--- config/connection.js
+++ config/connection.js
@@ -1,36 +1,31 @@
-const mysql = require("mysql")
-
-    var con = {}
-    if ((process.env.PORT || 3000) != 3000) {
-        con = {
-            //host: "us-cdbr-iron-east-02.cleardb.net",
-            host: "gmgcjwawatv599gq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-            port: 3306,
-           // user: "b813458a780ba2",
-           // password: "6c076ffa",
-             user: "yqr1358ag59b86t2",
-                 password: "o086x03u31yvacsx",
-           // database: "heroku_efc63d0c2f5b0a1",
-            database: "mfprp122v8lffehd"
-        }
-    } else {
-        con = {
-            host: "localhost",
-            port: 3306,
-            user: "root",
-            password: "Rcc083178",
-            database: "burgers_db"
-        }
-    }
-    console.log(con)
-var connection = mysql.createConnection(con)
-    console.log(connection)
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+require("dotenv").config();
+const private = require("./config.js");
+const mysql = require("mysql");
+var con = {};
+if ((process.env.PORT || 3000) != 3000) {
+  con = {
+    host: "gmgcjwawatv599gq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: private.mysqlPrivate.usernameRemote,
+    password: private.mysqlPrivate.passwordRemote,
+    database: "mfprp122v8lffehd"
+  };
+} else {
+  con = {
+    host: "localhost",
+    port: 3306,
+    user: private.mysqlPrivate.usernameLocal,
+    password: private.mysqlPrivate.passwordLocal,
+    database: "burgers_db"
+  };
+}
+var connection = mysql.createConnection(con);
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
- module.exports = connection
+module.exports = connection;
